3f2f77d6a2873a506877e78a1d59de91bc948232
Webprojon <tokhirjonkhasanov@gmail.com>
2025-06-08 10:51:48 +0200

    Navbar is changed

--- frontend/src/components/header/Navbar.tsx
+++ frontend/src/components/header/Navbar.tsx
@@ -5,11 +5,10 @@ import CreateButton from "./CreateButton";
 import Dropdown from "./Dropdown";
 import { useUserData } from "../../hooks/useUserData";
 import Language from "./Language";
-import { useAuthStore } from "../../store/authStore";
 
 export default function Navbar() {
 	const { isLoading } = useUserData();
-	const { token } = useAuthStore();
+	const refreshToken = localStorage.getItem("refreshToken");
 
 	return (
 		<header className="max-w-[1350px] mx-auto flex items-center justify-between h-[9vh] border-b px-2 xl:px-0 select-none">
@@ -17,7 +16,7 @@ export default function Navbar() {
 			<nav className="font-semibold flex items-center gap-x-5 sm:gap-x-3">
 				<CreateButton />
 				<Language />
-				{token ? (
+				{refreshToken ? (
 					<>{isLoading ? <Loader className="w-9 h-9" /> : <Dropdown />}</>
 				) : (
 					<Link to="/sign-in" className="py-[7px] sm:py-[4px] px-5 btn relative flex justify-center items-center">
