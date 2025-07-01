4b6528fb1453194967dc8cf5cef07d9210a258e8
Webprojon <tokhirjonkhasanov@gmail.com>
2025-07-01 09:31:03 +0200

    style: changed

--- frontend/src/components/input-components/AuthLayout.tsx
+++ frontend/src/components/input-components/AuthLayout.tsx
@@ -8,12 +8,12 @@ export default function AuthLayout({ subtitle, children }: AuthLayoutProps) {
 	return (
 		<motion.section initial="initial" animate="animate" variants={smoothOpacity} className="min-h-screen flex-center">
 			<div className="flex-center flex-wrap gap-10">
-				<div className="flex-center flex-col sm:gap-y-4 flex-1">
+				<div className="flex-center flex-col sm:gap-y-4 flex-1 text-slate-300 tracking-wider">
 					<Link to="/" className="flex-center gap-3">
 						<FaPeopleGroup className="size-14 text-sky-300" />
 						<h1 className="leading-none font-semibold text-4xl md:text-5xl text-center">Eventura</h1>
 					</Link>
-					<p className="text-xl tracking-wider text-slate-300">{subtitle}</p>
+					<p className="text-xl">{subtitle}</p>
 				</div>
 
 				<div className="hidden md:block h-135 border-r"></div>
--- frontend/src/pages/Starter.tsx
+++ frontend/src/pages/Starter.tsx
@@ -10,7 +10,7 @@ export default function StarterPage() {
 			<motion.div initial="initial" animate="animate" variants={fromTopToBottom} className="flex-center flex-col gap-y-4">
 				<div className="flex items-center gap-4">
 					<FaPeopleGroup className="size-12 text-sky-300" />
-					<h1 className="leading-none font-semibold text-5xl">Eventura</h1>
+					<h1 className="leading-none font-semibold text-5xl text-slate-200">Eventura</h1>
 				</div>
 				<p className="text-lg text-slate-300 text-center px-2 max-w-lg">Your gateway to discovering and joining amazing events around you.</p>
 				<Link to="/events" className="flex-center gap-x-2 group mt-6 py-2 px-12 btn">
